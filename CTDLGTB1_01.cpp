1b99309c865c8f515e2da27ecdc4e2f58e85bb77
nguyễn nhật <minhnhat12a7k33@gmail.com>
2023-03-30 09:15:44 +0700

    add

---
+++ CTDLGTB1_01.cpp
@@ -0,0 +1,33 @@
+//Cho mảng 1 chiều các số nguyên. Viết hàm tính tổng các số chẵn trong mảng bằng phương pháp đệ quy
+#include<iostream>
+
+using namespace std;
+
+const int Max=100;
+
+int tinhtong(int a[], int n){
+	if (n == 0)
+		return 0;
+	if (a[n] % 2 == 0)
+		return tinhtong(a, n - 1) + a[n];
+	else
+		return tinhtong(a, n - 1);
+}
+void xuattong(int a[], int n){
+	for(int i=1; i<=n; i++){
+		cout<<a[i]<<" ";
+	}
+}
+int main() {
+	int n;
+	int a[Max];
+	cout << "Nhap kich thuoc mang: ";
+	cin >> n;
+	for (int i = 1; i <= n;i++) {
+		cout << "Nhap a[" << i << "]= ";
+		cin >> a[i];
+	}
+	xuattong(a,n);
+	cout << "\nVay tong chan la: " << tinhtong(a,n)<<endl;;
+	return 0;
+}
